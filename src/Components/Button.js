0c92af3341a04677cc6e7cd38c6813b24f207e16
Yash <yashchavare1@gmail.com>
2024-07-02 01:07:39 +0530

    Completed with Button Search

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { YOUTUBE_CONTENT_SPECIFIED } from '../utils/constants'
+import { useDispatch } from 'react-redux';
+import { storeVideos } from '../utils/VideoSlice';
 
-const Button = ({name}) => {
+const Button = ({ text }) => {
+  const dispatch = useDispatch();
+
+  const handleContentSearch = async() => {
+    console.log(text);
+    const data = await fetch(YOUTUBE_CONTENT_SPECIFIED + text);
+    const json = await data.json();
+    dispatch(storeVideos(json.items));
+  }
+
   return (
     <div>
-        <button className='px-5 py-2 m-2 bg-gray-200 rounded-lg font-semibold hover:bg-gray-300'>{ name }</button>
+      <button onClick={handleContentSearch} className='px-5 py-2 h-[6vmin] m-2 bg-gray-200 rounded-lg font-semibold hover:bg-gray-300'>{text}</button>
     </div>
   )
 }
